d30220292dbd4628217c006d00d8e5256a28c9fb
Jakub Chojna <jakub.chojna@gmail.com>
2019-07-17 22:03:27 +0200

    feat: html code of grid content  generated in JS using my own html generator

---
+++ other scripts/html generator/script.js
@@ -0,0 +1,439 @@
+const portfolio = [
+  {
+    name: "Holiday Let",
+    viewbox: "0 0 1000 544",
+    src: "#holiday-let",
+    file: "holiday-let.jpg",
+    alt: "Two holiday lets in the Sussex countryside, UK | client: SG Architecture"
+  },
+  {
+    name: "General Aviation Terminal",
+    viewbox: "0 0 1000 640",
+    src: "#general-aviation-01",
+    file: "general-aviation-terminal-01.jpg",
+    alt: "General Aviation Terminal Concept in Góraszka, Poland | Bachelor Thesis on WAPW"
+  },
+  {
+    name: "Science Centre",
+    viewbox: "0 0 1000 1291",
+    src: "#science-centre-01",
+    file: "science-centre-ljubljana-01.jpg",
+    alt: "Science Centre in Ljubljana, Slovenia | client: Terroir"
+  },
+  {
+    name: "Kindergarten",
+    viewbox: "0 0 1000 1297",
+    src: "#kindergarten-04",
+    file: "kindergarten-kocevje-slovenia-04.jpg",
+    alt: "Kindergarten competition in Kočevje, Slovenia | 1st Prize | client: Svet Vmes"
+  },
+  {
+    name: "Faculty of Mechanical Engineering",
+    viewbox: "0 0 1000 448",
+    src: "#faculty-buildings-01",
+    file: "faculty-buildings-ljubljana-01.jpg",
+    alt: "Faculty of Mechanical Engineering in Ljubljana, Slovenia | client: Svet Vmes & Terroir"
+  },
+  {
+    name: "Faculty of Pharmacy",
+    viewbox: "0 0 1000 448",
+    src: "#faculty-buildings-02",
+    file: "faculty-buildings-ljubljana-02.jpg",
+    alt: "Faculty of Pharmacy in Ljubljana, Slovenia | client: Svet Vmes & Terroir"
+  },
+  {
+    name: "Water Tower",
+    viewbox: "0 0 1000 500",
+    src: "#watertower-01",
+    file: "watertower-denmark-01.jpg",
+    alt: "Water Tower in Denmark - view on the concrete structure | client: AFI"
+  },
+  {
+    name: "Parametric Pavilion",
+    viewbox: "0 0 1000 1000",
+    src: "#parametric-pavilion-01",
+    file: "parametric-pavilion-01.jpg",
+    alt: "Parametric Pavilion generated in Grasshopper and Kangaroo | personal work"
+  },
+  {
+    name: "Bramley Road",
+    viewbox: "0 0 1000 460",
+    src: "#bramley-road",
+    file: "bramley-road-renovation.JPG",
+    alt: "Interior renovation at Bramley Rd, London, UK | client: SG Architecture"
+  },
+  {
+    name: "Zodiak Square",
+    viewbox: "0 0 1000 1000",
+    src: "#zodiak-square-01",
+    file: "zodiak-square-warsaw-01.jpg",
+    alt: "Zodiak Square Concept in Warsaw, Poland | competition entry"
+  },
+  {
+    name: "Footbridge",
+    viewbox: "0 0 1000 550",
+    src: "#footbridge-01",
+    file: "footbridge-soca-river-slovenia-01.jpg",
+    alt: "Footbridge over the Soca River in Slovenia | client: Svet Vmes"
+  },
+  {
+    name: "LightWeb",
+    viewbox: "0 0 1000 500",
+    src: "#clue-light-01",
+    file: "clue-light-competition-01.jpg",
+    alt: "LightWeb light installation | Clue Light competition entry"
+  },
+  {
+    name: "Kindergarten",
+    viewbox: "0 0 1000 583",
+    src: "#kindergarten-01",
+    file: "kindergarten-kocevje-slovenia-01.jpg",
+    alt: "Kindergarten competition in Kočevje, Slovenia | 1st Prize | client: Svet Vmes"
+  },
+  {
+    name: "h2ouse",
+    viewbox: "0 0 1000 1000",
+    src: "#h2ouse-03",
+    file: "h2ouse-dom-na-wodzie-03.JPG",
+    alt: "h2Ouse - house on the water for flood victims | personal work"
+  },
+  {
+    name: "Quick Park Konstancin",
+    viewbox: "0 0 1000 540",
+    src: "#quick-park-konstancin-01",
+    file: "quick-park-konstancin-01.jpg",
+    alt: "Quick Park near Konstancin-Jeziorna, Poland | foodcourt zone | client: Mofo Architekci"
+  },
+  {
+    name: "Quick Park Konstancin",
+    viewbox: "0 0 1000 540",
+    src: "#quick-park-konstancin-02",
+    file: "quick-park-konstancin-02.jpg",
+    alt: "Quick Park near Konstancin-Jeziorna, Poland | foodcourt zone | client: Mofo Architekci"
+  },
+  {
+    name: "Water Tower",
+    viewbox: "0 0 1000 435",
+    src: "#watertower-02",
+    file: "watertower-denmark-02.jpg",
+    alt: "Water Tower in Denmark - view on the concrete structure | client: AFI"
+
+  },
+  {
+    name: "Public Space Furniture",
+    viewbox: "0 0 1000 1000",
+    src: "#public-space-furniture-01",
+    file: "public-space-furniture-01.jpg",
+    alt: "Signpost - Furniture in Public Space | prototype created for Gdynia Weekend of Architecture | in collaboration with Piotr Gniewek"
+  },
+  {
+    name: "Elementary Shapes",
+    viewbox: "0 0 1000 1000",
+    src: "#elementary-shapes-01",
+    file: "elementary-shapes-01.jpg",
+    alt: "7 Frames - Furniture in Public Space | prototype created for Gdynia Weekend of Architecture | in collaboration with Piotr Gniewek"
+  },
+  {
+    name: "The Runway",
+    viewbox: "0 0 1000 400",
+    src: "#the-runway",
+    file: "the-runway.jpg",
+    alt: "The Runway - Aviation Museum in Finland | client: AFI"
+  },
+  {
+    name: "Hotel & Spa",
+    viewbox: "0 0 1000 560",
+    src: "#hotel-spa-irena-eris",
+    file: "hotel-spa-irena-eris-01.jpg",
+    alt: "Pool in the Hotel & Spa Irena Eris | client: Atelier 3 Girtler & Girtler"
+  },
+  {
+    name: "Toruń",
+    viewbox: "0 0 1000 600",
+    src: "#shopping-gallery-torun-02",
+    file: "shopping-gallery-torun-02.jpg",
+    alt: "Shopping Mall in Toruń, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Kindergarten",
+    viewbox: "0 0 1000 820",
+    src: "#kindergarten-03",
+    file: "kindergarten-kocevje-slovenia-03.jpg",
+    alt: "Kindergarten competition in Kočevje, Slovenia | 1st Prize | client: Svet Vmes"
+
+  },
+  {
+    name: "Bathroom",
+    viewbox: "0 0 1000 775",
+    src: "#bathroom-01",
+    file: "bathroom-01.jpg",
+    alt: "Bathroom interior design in retro style | client: Paulina Dziedzic"
+  },
+  {
+    name: "Coworking Space",
+    viewbox: "0 0 1000 540",
+    src: "#coworking-space-01",
+    file: "coworking-space-praga-warsaw-01.jpg",
+    alt: "Coworking Space Concept in Warsaw, Poland | Master Thesis on WAPW"
+  },
+  {
+    name: "Kindergarten",
+    viewbox: "0 0 1000 482",
+    src: "#kindergarten-02",
+    file: "kindergarten-kocevje-slovenia-02.jpg",
+    alt: "Kindergarten competition in Kočevje, Slovenia | 1st Prize | client: Svet Vmes"
+  },
+  {
+    name: "Public Space Furniture",
+    viewbox: "0 0 1000 1083",
+    src: "#public-space-furniture-opole-02",
+    file: "public-space-furniture-opole-02.jpg",
+    alt: "Public furniture concept located along famous walk of fame in Opole, Poland | 4th prize at the competition | in collaboration with Piotr Gniewek"
+
+  },
+  {
+    name: "Footbridge",
+    viewbox: "0 0 1000 1000",
+    src: "#footbridge-03",
+    file: "footbridge-soca-river-slovenia-03.jpg",
+    alt: "Footbridge over the Soca River in Slovenia | client: Svet Vmes"
+  },
+  {
+    name: "h3ouse",
+    viewbox: "0 0 1000 818",
+    src: "#h2ouse-01",
+    file: "h2ouse-on-water-01.jpg",
+    alt: "h2Ouse - house on the water for flood victims | personal work"
+  },
+  {
+    name: "h3ouse",
+    viewbox: "0 0 1000 818",
+    src: "#h2ouse-02",
+    file: "h2ouse-on-water-02.jpg",
+    alt: "h2Ouse - house on the water for flood victims | personal work"
+  },
+  {
+    name: "Science Centre",
+    viewbox: "0 0 1000 626",
+    src: "#science-centre-02",
+    file: "science-centre-ljubljana-02.JPG",
+    alt: "Science Centre in Ljubljana, Slovenia | client: Terroir"
+  },
+  {
+    name: "General Aviation Terminal",
+    viewbox: "0 0 1000 1000",
+    src: "#general-aviation-02",
+    file: "general-aviation-terminal-02.jpg",
+    alt: "General Aviation Terminal Concept in Góraszka, Poland | Bachelor Thesis on WAPW"
+  },
+  {
+    name: "Science Centre",
+    viewbox: "0 0 1000 500",
+    src: "#science-centre-03",
+    file: "science-centre-ljubljana-03.jpg",
+    alt: "Science Centre in Ljubljana, Slovenia | client: Terroir"
+  },
+  {
+    name: "Residential Building",
+    viewbox: "0 0 1000 667",
+    src: "#residential-building-01",
+    file: "residential-building-rakowicka-warsaw-01.jpg",
+    alt: "Courtyard of residential building Rakowicka in Warsaw, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Public Space Furniture",
+    viewbox: "0 0 1000 657",
+    src: "#public-space-furniture-opole-01",
+    file: "public-space-furniture-opole-01.jpg",
+    alt: "Public furniture concept located along famous walk of fame in Opole, Poland | 4th prize at the competition | in collaboration with Piotr Gniewek"
+  },
+  {
+    name: "Parametric Pavilion",
+    viewbox: "0 0 1000 1000",
+    src: "#parametric-pavilion-02",
+    file: "parametric-pavilion-02.jpg",
+    alt: "Parametric Pavilion generated in Grasshopper and Kangaroo | personal work"
+  },
+  {
+    name: "Zodiak Square",
+    viewbox: "0 0 1000 756",
+    src: "#zodiak-square-02",
+    file: "zodiak-square-warsaw-02.jpg",
+    alt: "Zodiak Square Concept in Warsaw, Poland | competition entry"
+  },
+  {
+    name: "Frankfurt am Main",
+    viewbox: "0 0 1000 667",
+    src: "#frankfurt-am-main-01",
+    file: "frankfurt-am-main-01.jpg",
+    alt: "Urban planning design in Frankfurt am Main, Germany | client: Bartłomiej Matuszewski"
+  },
+  {
+    name: "Libero Gallery",
+    viewbox: "0 0 1000 600",
+    src: "#gallery-libero",
+    file: "gallery-libero-katowice.jpg",
+    alt: "Shopping Center in Katowice, Poland | relax zone | client: Mofo Architekci"
+  },
+  {
+    name: "Residential Building",
+    viewbox: "0 0 1000 667",
+    src: "#residential-building-02",
+    file: "residential-building-rakowicka-warsaw-02.jpg",
+    alt: "Exterior of residential building Rakowicka in Warsaw, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Public Space Furniture",
+    viewbox: "0 0 1000 625",
+    src: "#public-space-furniture-02",
+    file: "public-space-furniture-gdynia-02.jpg",
+    alt: "Signpost - Furniture in Public Space | prototype created for Gdynia Weekend of Architecture | in collaboration with Piotr Gniewek"
+  },
+  {
+    name: "Tomaszów Mazowiecki",
+    viewbox: "0 0 1000 700",
+    src: "#tomaszow-mazowiecki-01",
+    file: "tomaszow-mazowiecki-shopping-gallery-01.jpg",
+    alt: "Shopping Center in Tomaszów Mazowiecki, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Quick Park Konstancin",
+    viewbox: "0 0 1000 685",
+    src: "#quick-park-konstancin-05",
+    file: "quick-park-konstancin-05.jpg",
+    alt: "Quick Park near Konstancin-Jeziorna, Poland | interior | client: Mofo Architekci"
+  },
+  {
+    name: "Tennis Centre",
+    viewbox: "0 0 1000 500",
+    src: "#tennis-center-konstancin-01",
+    file: "tennis-center-konstancin-01.jpg",
+    alt: "Tennis Centre near Konstancin-Jeziorna, client: Mofo Architekci"
+  },
+  {
+    name: "Quick Park Konstancin",
+    viewbox: "0 0 1000 700",
+    src: "#quick-park-konstancin-04",
+    file: "quick-park-konstancin-04.jpg",
+    alt: "Quick Park near Konstancin-Jeziorna, Poland | foodcourt zone | client: Mofo Architekci"
+  },
+  {
+    name: "Wiślanka Gallery",
+    viewbox: "0 0 1000 676",
+    src: "#gallery-wislanka-01",
+    file: "gallery-wislanka-zory-01.jpg",
+    alt: "Wiślanka shopping gallery in Żory, Poland | client: Mofo Architekci"
+
+  },
+  {
+    name: "Bedroom",
+    viewbox: "0 0 1000 750",
+    src: "#bedroom-01",
+    file: "bedroom-01.jpg",
+    alt: "Bedroom | client: Ola Wołczyk-Perłak"
+
+  },
+  {
+    name: "Bedroom",
+    viewbox: "0 0 1000 707",
+    src: "#bedroom-02",
+    file: "bedroom-02.jpg",
+    alt: "Bedroom | client: Ola Wołczyk-Perłak"
+  },
+  {
+    name: "Quick Park Turek",
+    viewbox: "0 0 1000 634",
+    src: "#quick-park-turek-02",
+    file: "quick-park-turek-02.jpg",
+    alt: "Quick Park in Turek at night, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Bargework",
+    viewbox: "0 0 1000 625",
+    src: "#bargework-01",
+    file: "bargework-office-on-water-01.jpg",
+    alt: "Bargework - office on the water competition | honourable mention"
+  },
+  {
+    name: "Quick Park Turek",
+    viewbox: "0 0 1000 520",
+    src: "#quick-park-turek-01",
+    file: "quick-park-turek-01.jpg",
+    alt: "Quick Park in Turek, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Sport Hall",
+    viewbox: "0 0 1000 493",
+    src: "#sport-hall-01",
+    file: "sport-hall-polkowice-01.jpg",
+    alt: "Sport hall concept in Polkowice, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Color Park",
+    viewbox: "0 0 1000 520",
+    src: "#color-park-01",
+    file: "color-park-nowy-targ-01.jpg",
+    alt: "Color Park Shopping Mall in Nowy Targ, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Underground Passage",
+    viewbox: "0 0 1000 700",
+    src: "#underground-pedestrian-passage-02",
+    file: "underground-pedestrian-passage-warsaw-02.jpg",
+    alt: "Pedestrian underpass renovation concept in Warsaw city centre, Poland | client: Natalia Wilczak"
+  },
+  {
+    name:  "Tomaszów Mazowiecki",
+    viewbox: "0 0 1000 674",
+    src: "#tomaszow-mazowiecki-02",
+    file: "tomaszow-mazowiecki-shopping-gallery-02.jpg",
+    alt: "Shopping Center in Tomaszów Mazowiecki, Poland | client: Mofo Architekci"
+  },
+  {
+    name: "Quick Park Konstancin",
+    viewbox: "0 0 1000 563",
+    src: "#quick-park-konstancin-03",
+    file: "quick-park-konstancin-03.jpg",
+    alt: "Quick Park near Konstancin-Jeziorna, Poland | aerial view | client: Mofo Architekci"
+  },
+  {
+    name: "Toruń",
+    viewbox: "0 0 1000 427",
+    src: "#torun-shopping-gallery-01",
+    file: "torun-shopping-gallery-01.jpg",
+    alt: "Shopping Mall in Toruń, Poland | client: Mofo Architekci"
+  }
+]
+
+const root = document.querySelector('.root');
+
+const generateHTML = (obj) => {
+  let generatedHTML = "";
+  for (let i = 0; i < obj.length; i++) {
+    const name = obj[i].name;
+    const viewbox = obj[i].viewbox;
+    const src = obj[i].src;
+    const file = obj[i].file;
+    const alt = obj[i].alt;
+
+    generatedHTML += 
+    `<!-- ${name.toUpperCase()} -->
+    <section class="grid__item grid__item--js">
+      <h4 class="grid__title grid__title--js">${name}</h4>
+      <a href="#" class="grid__link">
+        <svg class="grid__placeholder grid__placeholder--js" viewBox="${viewbox}">
+          <use href="assets/svg/placeholders.svg${src}" class="grid__svg grid__svg--js"/>
+        </svg>
+        <img 
+          data-src="assets/img/960px/${file}"
+          data-src2="assets/img/1920px/${file}"
+          class="grid__image grid__image--js"
+          alt="${alt}">
+      </a>
+    </section>`
+
+    root.textContent = generatedHTML;
+  }
+}
+
+generateHTML(portfolio);
