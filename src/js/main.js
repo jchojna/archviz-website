096b03c42ceb6ddd5c0cba782fb03a028e4a4137
Jakub Chojna <jakub.chojna@gmail.com>
2019-07-18 21:52:25 +0200

    chore: page overall scss setup

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log('Welcome!');
+const pageOverlay = document.querySelector('.page-overlay--js');
+
+const fadeInPage = () => {
+  pageOverlay.classList.remove('page-overlay--onload');
+}
+
+window.onload = () => {
+  fadeInPage();
+}
