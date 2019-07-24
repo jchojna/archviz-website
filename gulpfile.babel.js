da2610fdc8ac5b6d2c9ab2be16d0efca4c73bcb5
Jakub Chojna <jakub.chojna@gmail.com>
2019-07-24 15:45:07 +0200

    chore: gulp starter kit update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,6 @@
 "use strict";
 
 import gulp from "gulp";
-import babel from "gulp-babel";
-import uglify from "gulp-uglify";
 import browserSync from "browser-sync";
 import sass from "gulp-sass";
 import autoprefixer from "autoprefixer";
@@ -10,18 +8,16 @@ import postcss from "gulp-postcss";
 import sourcemaps from "gulp-sourcemaps";
 import notify from "gulp-notify";
 import plumber from "gulp-plumber";
-import webpack from 'webpack';
-import webpackConfig from './webpack.config.js';
-import webpackStream from 'webpack-stream';
+import webpack from "webpack-stream";
 
 sass.compiler = require("node-sass");
 
-const errorHandler = (err) => {
-    notify.onError({
-      title: `Gulp error in ${err.plugin}`,
-      message: err.toString()
-    })(err);
-}
+const errorHandler = err => {
+  notify.onError({
+    title: `Gulp error in ${err.plugin}`,
+    message: err.toString()
+  })(err);
+};
 
 gulp.task("assets", function() {
   return gulp.src("./src/assets/**/*").pipe(gulp.dest("./dist/assets/"));
@@ -30,22 +26,30 @@ gulp.task("assets", function() {
 gulp.task("html", function() {
   return gulp
     .src("./src/content/**/*.html")
-    .pipe(
-      plumber(errorHandler)
-    )
+    .pipe(plumber(errorHandler))
+    .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("pwa", function() {
+  return gulp
+    .src("./src/pwa/**/*")
     .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task("js", function() {
-  return gulp
-    .src("src/js/main.js")
-    .pipe(
-      plumber(errorHandler)
-    )
-    .pipe(webpackStream(webpackConfig), webpack)
-    .pipe(uglify())
-    .on('error', errorHandler)
-    .pipe(gulp.dest("dist/js"));
+  return gulp.src('src/js')
+  .pipe(
+    plumber({
+      errorHandler: function(err) {
+        notify.onError({
+          title: `Gulp error in ${err.plugin}`,
+          message: err.toString()
+        })(err);
+      }
+    })
+  )
+  .pipe(webpack(require('./webpack.config.js')))
+  .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task("sass", () => {
@@ -76,18 +80,19 @@ gulp.task("sass", () => {
 
 gulp.task(
   "serve",
-  gulp.series("sass", "html", "js", "assets", function() {
+  gulp.series("sass", "html", "js", "assets", "pwa", function() {
     browserSync.init({
       server: "./dist",
       open: true // set to false to disable browser autostart
     });
     gulp.watch("src/scss/**/*", gulp.series("sass"));
     gulp.watch("src/content/**/*.html", gulp.series("html"));
+    gulp.watch("src/pwa/**/*", gulp.series("pwa"));
     gulp.watch("src/js/*.js", gulp.series("js"));
     gulp.watch("src/assets/**/*", gulp.series("assets"));
     gulp.watch("dist/**/*").on("change", browserSync.reload);
   })
 );
 
-gulp.task("build", gulp.series("sass", "html", "js", "assets"));
+gulp.task("build", gulp.series("sass", "html", "js", "assets", "pwa"));
 gulp.task("default", gulp.series("serve"));
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
+"use strict";
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 const pageOverlay = document.querySelector('.page-overlay--js');
 
 const fadeInPage = () => {
