415c4b167cb06c8f112997a11071f427352c9b84
Jakub Chojna <jakub.chojna@gmail.com>
2019-09-06 19:36:12 +0200

    fix(html): nofollow and noreferrer for links to be open in a new card

--- src/js/main.js
+++ src/js/main.js
@@ -143,6 +143,11 @@ ga("send", "pageview");
    ##    ##     ## ##     ##  #######     ##       ##    ######## ########
 */
 
+// Underscore.js 1.9.1
+// http://underscorejs.org
+// (c) 2009-2018 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+// Underscore may be freely distributed under the MIT license.
+
 const timestamp = () => {
   return new Date().getTime();
 };
