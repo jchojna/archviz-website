f59924293360ac79465c78f7c75842a2469b0bee
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-12 14:05:07 +0200

    style: change hover and click effect for info section on about page

--- src/js/main.js
+++ src/js/main.js
@@ -685,20 +685,23 @@ if (gallery) {
 if (about) {
   
   var minimizeCards = () => {
-    [...cardDescription].forEach(c=>c.style.marginTop=`${(-1)*c.clientHeight-3}px`);
-    [...cardDropdown].forEach(d=>d.classList.toggle('card__dropdown--reversed'));
+    [...cards].forEach(c=>c.classList.toggle('card--rolled-up'));
+    [...cardDescriptions].forEach(c=>c.style.marginTop=`${(-1)*c.clientHeight-3}px`);
+    [...cardDropdowns].forEach(d=>d.classList.toggle('card__dropdown--reversed'));
   }
 
   const handleCard = (e) => {
     const {index} = e.target;
-    const card = cardDescription[index];
-    const dropdown = cardDropdown[index];
+    const cardContainer = cards[index];
+    const card = cardDescriptions[index];
+    const dropdown = cardDropdowns[index];
     const {marginTop} = card.style;
 
     if (!card.classList.contains('card__description--transition')) {
       card.classList.add('card__description--transition');
       dropdown.classList.add('card__dropdown--transition');
     }
+    cardContainer.classList.toggle('card--rolled-up');
     card.style.marginTop = marginTop === "0px" || marginTop === ""
     ? `${(-1) * card.clientHeight - 3}px`
     : "0px";
@@ -706,17 +709,18 @@ if (about) {
   }
   
   const handleCardsOnResize = () => {
-    [...cardDescription].forEach(c => c.style.marginTop =
+    [...cardDescriptions].forEach(c => c.style.marginTop =
     c.style.marginTop === "0px" || c.style.marginTop === ""
     ? "0px"
     : `${(-1) * c.clientHeight - 3}px`);
   }
 
-  const cardHeader = document.querySelectorAll('.card__header--js');
-  const cardDescription = document.querySelectorAll('.card__description--js');
-  const cardDropdown = document.querySelectorAll('.card__dropdown--js');
+  const cards = document.querySelectorAll('.card--js');
+  const cardHeaders = document.querySelectorAll('.card__header--js');
+  const cardDescriptions = document.querySelectorAll('.card__description--js');
+  const cardDropdowns = document.querySelectorAll('.card__dropdown--js');
 
-  [...cardHeader].forEach((a,i) => {
+  [...cardHeaders].forEach((a,i) => {
     a.index = i;
     a.addEventListener('click', handleCard);
   });
