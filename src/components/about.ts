c9fc6431cc46cb65c7be902b4547f90ca609a16f
Jakub Chojna <jakub.chojna@gmail.com>
2024-03-16 19:38:45 +0100

    chore: create initial about and contact ts files

---
+++ src/components/about.ts
@@ -0,0 +1,294 @@
+<div class="about about--js" role="main">
+  <section class="info info--js">
+    <h2 class="info__heading visuallyhidden">Information</h2>
+    <section class="card card--js">
+      <header class="card__header card__header--js" role="banner">
+        <h3 class="card__heading visuallyhidden">About me</h3>
+        <button
+          class="button card__header-button card__header-button--js"
+          aria-label="Toggle about me section"
+        >
+          <span class="card__title">About Me</span>
+          <svg class="card__dropdown card__dropdown--js" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#dropdown"></use>
+          </svg>
+        </button>
+      </header>
+      <div class="card__description card__description--js">
+        <p class="card__text">
+          I am an architect and 3d graphics enthusiast, specializing in
+          <em class="card__emphasized">3d visualization</em> of architecture,
+          interiors and every form of design. While creating a new project I
+          focus on appropriate form of narration and
+          <em class="card__emphasized">story-telling</em>. Because of that a
+          rendering becomes something more than just a still image – a scene of
+          life, an inspiration, an art.
+        </p>
+        <a
+          href="/assets/pdf/architectural_portfolio.pdf"
+          class="button card__button"
+          target="_blank"
+          rel="nofollow noreferrer"
+          aria-label="Open architectural portfolio pdf"
+        >
+          ARCHITECTURAL PORTFOLIO
+        </a>
+      </div>
+    </section>
+    <section class="card card--js">
+      <header class="card__header card__header--js" role="banner">
+        <h3 class="card__heading visuallyhidden">Experience</h3>
+        <button
+          class="button card__header-button card__header-button--js"
+          aria-label="Toggle experience section"
+        >
+          <span class="card__title">Experience</span>
+          <svg class="card__dropdown card__dropdown--js" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#dropdown"></use>
+          </svg>
+        </button>
+      </header>
+      <div class="card__description card__description--js">
+        <p class="card__text">
+          I have been working in the field of visual presentation for
+          <em class="card__emphasized">over the last 10 years</em>. As a
+          graduate with a Master’s Degree in Architecture I have a strong
+          <em class="card__emphasized">architectural background</em> and
+          experience gained in design studios in Poland and abroad. Due to that,
+          I am fully aware how a project development process runs and how a good
+          cooperation between designer, visualizer and investor should look
+          like.
+        </p>
+        <a
+          href="/assets/pdf/archviz_portfolio.pdf"
+          class="button card__button"
+          target="_blank"
+          rel="nofollow noreferrer"
+          aria-label="Open archviz portfolio pdf"
+        >
+          ARCHVIZ PORTFOLIO
+        </a>
+      </div>
+    </section>
+    <section class="card card--js">
+      <header class="card__header card__header--js" role="banner">
+        <h3 class="card__heading visuallyhidden">Offer</h3>
+        <button
+          class="button card__header-button card__header-button--js"
+          aria-label="Toggle offer section"
+        >
+          <span class="card__title">Offer</span>
+          <svg class="card__dropdown card__dropdown--js" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#dropdown"></use>
+          </svg>
+        </button>
+      </header>
+      <div class="card__description card__description--js">
+        <ul class="card__text card__text--list">
+          <li class="card__list-item">architectural visualization</li>
+          <li class="card__list-item">commercial visualization</li>
+          <li class="card__list-item">product visualization, packshots</li>
+        </ul>
+      </div>
+    </section>
+    <section class="card card--js">
+      <header class="card__header card__header--js" role="banner">
+        <h3 class="card__heading visuallyhidden">Clients</h3>
+        <button
+          class="button card__header-button card__header-button--js"
+          aria-label="Toggle clients section"
+        >
+          <span class="card__title">Clients</span>
+          <svg class="card__dropdown card__dropdown--js" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#dropdown"></use>
+          </svg>
+        </button>
+      </header>
+      <div class="card__description card__description--js">
+        <p class="card__text">
+          Up till now I collaborated with many companies and private clients
+          from Poland, Great Britain, Australia, Denmark, Austria, Slovenia,
+          including
+          <em class="card__emphasized">architectural offices</em> and studios,
+          <em class="card__emphasized">real estate developers</em> and
+          <em class="card__emphasized">advertising agencies</em>.
+        </p>
+      </div>
+    </section>
+  </section>
+  <nav class="clients" role="navigation" aria-label="Clients websites">
+    <h2 class="clients__heading visuallyhidden">Clients</h2>
+    <ul class="clients__list">
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Konior Studio</h3>
+        <a
+          href="http://www.koniorstudio.pl/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Konior Studio website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#konior-studio"></use>
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Mofo Architekci</h3>
+        <a
+          href="http://mofo.pl/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Mofo Architekci website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#mofo" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Echo Investment</h3>
+        <a
+          href="https://www.echo.com.pl/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Echo Investment website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#echo" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Hackett Hamilton</h3>
+        <a
+          href="http://www.hacketthamilton.com/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Hackett Hamilton website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#hackett-hamilton" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Mallson</h3>
+        <a
+          href="http://www.mallson.pl/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Mallson website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#mallson" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Studio Thörnblom</h3>
+        <a
+          href="http://www.studio-thoernblom.at/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Studio Thörnblom website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#studio-thoernblom" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Terroir</h3>
+        <a
+          href="https://www.terroir.com.au/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Terroir website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#terroir" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Ola Wołczyk</h3>
+        <a
+          href="https://olawolczyk.pl/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Ola Wolczyk website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#olawolczyk" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Real2b Development</h3>
+        <a
+          href="http://real2bdevelopment.pl/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Real2b Development website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#real2b" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">
+          Atelier 3 Girtler & Girtler
+        </h3>
+        <a
+          href="http://atelier3.pl/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Atelier 3 Girtler & Girtler website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#atelier3" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">Svet Vmes</h3>
+        <a
+          href="https://www.svetvmes.si/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="Svet Vmes website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#svet-vmes" />
+          </svg>
+        </a>
+      </li>
+      <li class="clients__item">
+        <h3 class="clients__client visuallyhidden">PRES Deweloper</h3>
+        <a
+          href="http://pres.com.pl/"
+          target="_blank"
+          rel="nofollow noreferrer"
+          class="clients__link"
+          aria-label="PRES Deweloper website"
+        >
+          <svg class="clients__svg" viewBox="0 0 512 512">
+            <use href="assets/svg/icons.svg#pres" />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>
