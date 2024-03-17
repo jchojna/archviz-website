2905e36d41c28bd1e519740420a7ced3727471af
Jakub Chojna <jakub.chojna@gmail.com>
2024-03-17 20:18:46 +0100

    chore: remove current contact form implementation

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import './sass/main.scss';
 
 import { renderAbout } from './components/about';
 import { breakpoints } from './components/constants';
+import { renderContact } from './components/contact';
 import { renderFooter } from './components/footer';
 import { renderGallery } from './components/gallery';
 import { lazyLoad, renderGrid, setFlexBasis } from './components/grid';
 import { renderHeader } from './components/navbar';
 
 const about = document.querySelector('.about--js');
-const form = document.querySelector('.form--js');
 
 if (about) {
   var minimizeCards = () => {
@@ -64,197 +64,18 @@ if (about) {
   window.addEventListener('resize', handleCardsOnResize);
 }
 
-if (form) {
-  const resetCheckboxes = () => {
-    for (let i = 0; i < checkboxes.length; i++) {
-      const checkbox = checkboxes[i];
-      checkbox.disabled = false;
-      checkbox.checked = false;
-      checkbox.parentNode.classList.remove('checkbox--disabled');
-      checkbox.nextElementSibling.classList.remove('checkbox__field--hidden');
-      checkboxMarks[i].classList.remove('checkbox__mark--visible');
-    }
-  };
-
-  const toggleOption = (element) => {
-    element.disabled = element.disabled ? false : true;
-    element.parentNode.classList.toggle('checkbox--disabled');
-  };
-
-  const handleCheckboxes = (e) => {
-    const { index } = e.target;
-    const [reject, accept, optional] = checkboxes;
-    const [rejectSvg, acceptSvg, optionalSvg] = checkboxMarks;
-    checkboxMarks[index].classList.toggle('checkbox__mark--visible');
-
-    switch (index) {
-      case 0:
-        toggleOption(accept);
-        toggleOption(optional);
-        break;
-      case 1:
-        optional.checked = false;
-        optionalSvg.classList.remove('checkbox__mark--visible');
-        toggleOption(reject);
-        break;
-      case 2:
-        accept.checked = true;
-        acceptSvg.classList.add('checkbox__mark--visible');
-        reject.disabled ? false : toggleOption(reject);
-        break;
-    }
-
-    [...checkboxes].forEach((a) =>
-      a.checked
-        ? a.nextElementSibling.classList.add('checkbox__field--hidden')
-        : a.nextElementSibling.classList.remove('checkbox__field--hidden')
-    );
-  };
-
-  const validateForm = (e) => {
-    e.preventDefault();
-    // backend validation => send form using ajax request
-    const formSubmit = document.querySelector('.form__submit--js').value;
-    const userName = document.querySelector('.input__data--js-userName').value;
-    const userTitle = document.querySelector(
-      '.input__data--js-userTitle'
-    ).value;
-    const userEmail = document.querySelector(
-      '.input__data--js-userEmail'
-    ).value;
-    const userCountryCode = document.querySelector(
-      '.input__data--js-userCountryCode'
-    ).value;
-    const userPhone = document.querySelector(
-      '.input__data--js-userPhone'
-    ).value;
-    const userMessage = document.querySelector(
-      '.input__data--js-userMessage'
-    ).value;
-    const checkboxAccept = document.querySelector(
-      '.checkbox__input--js-accept'
-    ).checked;
-    const checkboxReject = document.querySelector(
-      '.checkbox__input--js-reject'
-    ).checked;
-
-    $.ajax({
-      url: 'form-handler.php',
-      type: 'post',
-      dataType: 'json',
-      data: {
-        submit: formSubmit,
-        userName: userName,
-        userTitle: userTitle,
-        userEmail: userEmail,
-        userCountryCode: userCountryCode,
-        userPhone: userPhone,
-        userMessage: userMessage,
-        checkboxAccept: checkboxAccept,
-        checkboxReject: checkboxReject,
-      },
-    })
-      .done((data) => handleAlerts(data, false))
-      .fail((data) => handleAlerts(data, true));
-    return;
-  };
-
-  const handleAlerts = (data, isFailed) => {
-    const {
-      emailError,
-      phoneError,
-      messageError,
-      checkboxError,
-      success,
-      failure,
-    } = data;
-
-    const toggleBoxAlert = () => {
-      alert.classList.add('alert--visible');
-      window.clearTimeout(alertTimeoutId);
-      alertTimeoutId = setTimeout(
-        () => alert.classList.remove('alert--visible'),
-        3000
-      );
-    };
-    const toggleInputAlert = (input, error) => {
-      if (error !== '') {
-        input.textContent = error;
-        input.classList.add('error__text--visible');
-      } else {
-        input.classList.remove('error__text--visible');
-      }
-    };
-    if (isFailed) {
-      alertMessage.textContent = 'Upss... something went wrongs...';
-      toggleBoxAlert();
-    } else {
-      // show box alert
-      if (success !== '' || failure !== '' || checkboxError !== '') {
-        const alert = [success, failure, checkboxError]
-          .filter((elem) => elem !== '')
-          .toString();
-        alertMessage.textContent = alert;
-        // hide input alerts
-        if (alert === success) {
-          [...errors].forEach((error) =>
-            error.classList.remove('error__text--visible')
-          );
-          [...formInputs].forEach((input) => (input.value = ''));
-          resetCheckboxes();
-        }
-        toggleBoxAlert();
-      }
-      // show input(s) alert
-      toggleInputAlert(errorPhone, phoneError);
-      toggleInputAlert(errorEmail, emailError);
-      toggleInputAlert(errorMessage, messageError);
-    }
-  };
-
-  ////////////////////////////////////////////////////////////////// VARIABLES
-  const checkboxes = document.querySelectorAll('.checkbox__input--js');
-  const checkboxMarks = document.querySelectorAll('.checkbox__mark--js');
-  const formInputs = document.querySelectorAll('.input__data--js');
-
-  const errors = document.querySelectorAll('[class*="error__text--js"]');
-  const errorEmail = document.querySelector('.error__text--js-email');
-  const errorPhone = document.querySelector('.error__text--js-phone');
-  const errorMessage = document.querySelector('.error__text--js-message');
-  const formSubmitButton = document.querySelector('.form__submit--js');
-
-  const alert = document.querySelector('.alert--js');
-  const alertMessage = document.querySelector('.alert__message--js');
-  const alertClose = document.querySelector('.alert__button--js-close');
-  let alertTimeoutId;
-  //////////////////////////////////////////////////////////// EVENT LISTENERS
-  formSubmitButton.addEventListener('click', validateForm);
-  alertClose.addEventListener('click', () => {
-    alert.classList.remove('alert--visible');
-  });
-
-  [...checkboxes].forEach((checkbox, index) => {
-    checkbox.index = index;
-    checkbox.addEventListener('click', handleCheckboxes);
-  });
-  /////////////////////////////////////////////////////// HANDLE LOCAL STORAGE
-
-  // CONTACT FORM LOCAL STORAGE FUNCTIONALITY
-  /*
-  [...formInputs].forEach(input => {
-    const key = `jc-${input.getAttribute('id')}`;
-    input.value = localStorage.getItem(key) ? localStorage.getItem(key) : "";
-    input.addEventListener('keyup', () => localStorage.setItem(key, input.value));
-  });
-  */
-}
-
 window.onload = () => {
   renderHeader();
   renderGrid();
   renderGallery();
   renderFooter();
   renderAbout();
+
+  // renderContact();
+  const contactContainer = document.querySelector('.contact--js');
+  console.log(contactContainer);
+  contactContainer.innerHTML = renderContact();
+  // document.querySelector('.contact--js')?.innerHTML = renderContact();
 
   setFlexBasis();
   window.addEventListener('resize', setFlexBasis);
